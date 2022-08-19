cedd5d376a3ed9ec34388f5db675a0876ecc2b64
Frolaf <78124286+Frolaf@users.noreply.github.com>
2022-08-19 11:41:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+    clown_nose= loadImage('https://i.postimg.cc/wvH0r3P8/Clown-Nose-PNG-Pic.png');
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
